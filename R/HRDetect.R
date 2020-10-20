3688a4d4d146daec18e8d650dbad6214cf059ef3
Andrea Degasperi <ad923@cam.ac.uk>
2020-10-20 16:24:02 +0100

    Follow up fix, improved warning message for HRDetect_pipeline rearrangement bedpe file having more than one sample

--- R/HRDetect.R
+++ R/HRDetect.R
@@ -328,10 +328,12 @@ HRDetect_pipeline <- function(data_matrix=NULL,
         reslist <- bedpeToRearrCatalogue(sv_bedpe)
         res <- reslist$rearr_catalogue
         # check that only one catalogue is generated. If not, take the one with more mutatations and raise a warning
-        if(ncol(res)>1){
+        resncol <- ncol(res)
+        if(resncol>1){
           rescolsum <- apply(res,2,sum)
-          res <- res[,which.max(rescolsum)[1],drop=F]
-          message(paste0("[warning HRDetect_pipeline] BEDPE file for sample ",sample," contained two sample names. This could be due to germline rearrangements that should be removed. Using only the sample with the largest number of rearrangments. Please double check and rerun if necessary with only one sample for each BEDPE file."))
+          sampletouse <- which.max(rescolsum)[1]
+          message(paste0("[warning HRDetect_pipeline] BEDPE file for sample ",sample," contained ",resncol," sample names: ",paste(colnames(res),collapse = ", "),". This could be due to germline rearrangements that should be removed. Using only the sample with the largest number of rearrangments (",colnames(res)[sampletouse],"). Please double check and rerun if necessary with only one sample for each BEDPE file."))
+          res <- res[,sampletouse,drop=F]
         }
         colnames(res) <- sample
         res
